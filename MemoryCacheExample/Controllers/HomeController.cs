2ae916923f08be1751ae70ac98a8d3253da61a00
Harun Değer <harun.deger@nesine.com>
2021-01-09 14:54:46 +0300

    set ve get metodları eklendi

--- MemoryCacheExample/Controllers/HomeController.cs
+++ MemoryCacheExample/Controllers/HomeController.cs
@@ -6,23 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MemoryCacheExample.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace MemoryCacheExample.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _memoryCache;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IMemoryCache memoryCache)
         {
             _logger = logger;
+            _memoryCache = memoryCache;
         }
 
         public IActionResult Index()
         {
+            _memoryCache.Set<string>("Date",DateTime.Now.ToString());
             return View();
         }
 
+        public IActionResult Show()
+        {
+            var dateInfo = _memoryCache.Get<string>("Date");
+            ViewBag.dateInfo = dateInfo;
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
